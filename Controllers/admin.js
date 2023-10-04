5180ad031ff9cbeba165fc612fcc9bff5f9115c7
Dharvik19 <dharvik.badga22@gmail.com>
2023-10-04 17:49:58 +0530

    Retrieving data from databases

--- Controllers/admin.js
+++ Controllers/admin.js
@@ -16,8 +16,11 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null,title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+  product.save()
+  .then(()=>{
+    res.redirect('/');
+  })
+  .catch(err=> console.log(err));
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -73,5 +76,6 @@ exports.getProducts = (req, res, next) => {
 
 exports.deletrProducts=(req, res, next)=>{
   const prodId = req.body.productId;
-  
+  Product.deleteById(prodId);
+  res.redirect('/admin/products');
 }
